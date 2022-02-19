4105e17198e5490c67770fa7538c661de6e0404d
erikandersson96 <erik96.andersson@gmail.com>
2022-02-19 11:44:06 +0000

    Add querySelector for easy/medium/hard in js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const easyOne = document.querySelector('.easy-one'); 
+const easyTwo = document.querySelector('.easy-two'); 
+const easyThree = document.querySelector('.easy-three'); 
+const easyFour = document.querySelector('.easy-four'); 
+
+
+const mediumOne = document.querySelector('.medium-one'); 
+const mediumTwo = document.querySelector('.medium-two');
+const mediumThree = document.querySelector('.medium-three');
+const mediumFour = document.querySelector('.medium-four');
+const mediumFive = document.querySelector('.medium-five');
+const mediumSix = document.querySelector('.medium-six'); 
+
+
+const hardOne = document.querySelector('.hard-one'); 
+const hardTwo = document.querySelector('.hard-two'); 
+const hardThree = document.querySelector('.hard-three'); 
+const hardFour = document.querySelector('.hard-four'); 
+const hardFive = document.querySelector('.hard-five'); 
+const hardSix = document.querySelector('.hard-six'); 
+const hardSeven = document.querySelector('.hard-seven'); 
+const hardEight = document.querySelector('.hard-eight'); 
+
+
