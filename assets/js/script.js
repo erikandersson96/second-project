6faa9a5f9030c023aec57c28a860c01d2db0e489
erikandersson96 <erik96.andersson@gmail.com>
2022-02-21 12:40:39 +0000

    Add js code for startgame and game instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,14 @@ let sequenceEasy = [];
 let sequenceEasyHuman = []; 
 
 const easyStart = document.querySelector('.easy-start'); 
+const infoEasy = document.querySelector('.info-easy'); 
 
+function startEasyGame() {
+    infoEasy.classList.remove('hidden'); 
+    infoEasy.textContent = 'The computer will play a random sequence'; 
+} 
 
-easyStart.addEventListener('click', playGame); 
+easyStart.addEventListener('click', startGame); 
 
 
  
