3bbd35e60ab1b023297bbbc5f653586bb5f1ae05
erikandersson96 <erik96.andersson@gmail.com>
2022-02-21 13:00:12 +0000

    Add jc code for random sequence easy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+
 // Array for computer sequence and human sequence, Easy difficulty level
 let sequenceEasy = []; 
 let sequenceEasyHuman = []; 
+let level = 0; 
 
 const easyStart = document.querySelector('.easy-start'); 
 const infoEasy = document.querySelector('.info-easy'); 
@@ -12,7 +14,16 @@ function startEasyGame() {
 
 easyStart.addEventListener('click', startGame); 
 
+function nextPaceEasy() {
+    const circleEasy = ['easy-one', 'easy-two', 'easy-three', 'easy-four']; 
+    const randomEasy = circleEasy[Math.floor(Math.random() * circleEasy.length)];
+}
 
+function nextRoundEasy() {
+    level += 1; 
+
+    const nextSequenceEasy = ['easy-one', 'easy-two', 'easy-three', 'easy-four'] 
+}; 
  
 
 
