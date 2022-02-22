a4afacc51daa0864823f662b2554a5e5e4065ddf
erikandersson96 <erik96.andersson@gmail.com>
2022-02-22 13:33:20 +0000

    Change js code & styling circles easy/medium/hard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,36 @@
 
-// Array for computer sequence and human sequence. Easy difficulty level
-let sequenceEasy = ['easy-one', 'easy-two', 'easy-three', 'easy-four']; 
-let sequenceEasyHuman = []; 
-let level = 0; 
+const easyOne = document.querySelector('.easy-one');
+const easyTwo = document.querySelector('.easy-two');
+const easyThree = document.querySelector('.easy-three');
+const easyFour = document.querySelector('.easy-four'); 
 
-const easyStart = document.querySelector('.easy-start'); 
-const infoEasy = document.querySelector('.info-easy'); 
+const sequenceEasy = [easyOne, easyFour, easyTwo, easyThree]; 
 
-// This function makes sure that 
-function flashCircleEasy(circle) {
-    const circleEasy = document.querySelector(`[data-circle-easy='${circle}']`); 
-
-    circleEasy.classList.add('flash'); 
-
-    setTimeout(() => {
-        circleEasy.classList.remove('flash'); 
-    }, 400); 
-} 
-
-// This function make sure that each circle flashes with a delay between each other in the sequence
-function playSequence(nextSequenceEasy) {
-    nextSequenceEasy.forEach((circle, index) => {
+const flashEasy = (easy) => {
+    return new Promise((resolve, reject) => {
+        easy.className += ' active';
         setTimeout(() => {
-            flashCircleEasy(circle);
-        }, (index + 1) * 700); 
-    }); 
+          easy.className = easy.className.replace(
+           ' active', 
+           ''
+        ); 
+           resolve(); 
+        }, 800)
+    });
+ };
+ 
+
+const mainEasy = async () => {
+    for (let easy of sequenceEasy) {
+    await flashEasy(easy);    
+
+    }
+   
 }
 
+mainEasy(); 
 
-// This function makes sure that 
-function nextStepEasy() {
-    const circleEasy = ['easy-one', 'easy-two', 'easy-three', 'easy-four']; 
-    const randomEasy = circleEasy[Math.floor(Math.random() * circleEasy.length)];
-
-    return randomEasy; 
-}
-// This nested function makes sure that
-function nextRoundEasy() {
-    level += 1; 
-
-    const nextSequenceEasy = [...sequenceEasy]; 
-    nextSequenceEasy.push(nextStepEasy()); 
-    playSequence(nextStepEasy);
-} 
-
-// This function handles the information text above the game play
-function startEasyGame() {
-    infoEasy.classList.remove('hidden'); 
-    infoEasy.textContent = 'The computer will play a random sequence'; 
-    nextRoundEasy(); 
-} 
-
-easyStart.addEventListener('click', startEasyGame); 
-
-
-
+console.log(sequenceEasy); 
 
 
 
